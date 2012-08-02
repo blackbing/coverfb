0bb31163fd039223a943a40caad1d54727a20e2b
Bingo Yang <blackbing@gmail.com>
2012-08-02 19:48:40 -0400

    Initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoverfbNet::Application.config.secret_token = '2979166609f2e5c7e1d38d70d7965188e9dc4d8d8759d598e61e85d621da886839f25841882cd8080216d2cd132bd6f384b5aab3d4fb690a4b35330fa6de45eb'
