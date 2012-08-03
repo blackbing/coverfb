28ade00f799fba5f573832ef14609b0b4de6c1b9
Bingo Yang <blackbing@gmail.com>
2012-08-03 04:04:45 -0400

    template ok

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ CoverfbNet::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'pages#welcome'
 
   # See how all your routes lay out with "rake routes"
 
